f622d35f1d269146d3162e033723891340338ad6
ErikPerez312 <theerikperez@gmail.com>
2018-02-06 14:54:37 -0800

    Added Campaign model controller and sanitized parameters

---
+++ app/controllers/campaigns_controller.rb
@@ -0,0 +1,63 @@
+class CampaignsController < ApplicationController
+  before_action :set_campaign, only: [:show, :update, :destroy]
+
+  # GET /campaigns
+  def index
+    @campaigns = Campaign.all
+
+    render json: @campaigns
+  end
+
+  # GET /campaigns/1
+  def show
+    render json: @campaign
+  end
+
+  # POST /campaigns
+  def create
+    @campaign = Campaign.new(campaign_params)
+
+    if @campaign.save
+      render json: @campaign, status: :created, location: @campaign
+    else
+      render json: @campaign.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /campaigns/1
+  def update
+    if @campaign.update(campaign_params)
+      render json: @campaign
+    else
+      render json: @campaign.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /campaigns/1
+  def destroy
+    @campaign.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_campaign
+      @campaign = Campaign.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def campaign_params
+      params.permit(
+        :current_status,
+        :total_cost,
+        :exposure_view_count,
+        :video_focus_people,
+        :video_focus_product,
+        :video_duration,
+        :video_title,
+        :video_link_url,
+        :video_like_count,
+        :video_comment_count,
+        :video_share_count
+      )
+    end
+end
