b027f6f8e96e54cb2a2cd3ce8107ba69395f8fa6
ErikPerez312 <theerikperez@gmail.com>
2018-02-08 11:13:18 -0800

    Added VideoStyle model controller, routes, and sanitized parameters

---
+++ app/controllers/video_styles_controller.rb
@@ -0,0 +1,58 @@
+class VideoStylesController < ApplicationController
+  before_action :set_video_style, only: [:show, :update, :destroy]
+
+  # GET /video_styles
+  def index
+    @video_styles = VideoStyle.all
+
+    render json: @video_styles
+  end
+
+  # GET /video_styles/1
+  def show
+    render json: @video_style
+  end
+
+  # POST /video_styles
+  def create
+    @video_style = VideoStyle.new(video_style_params)
+
+    if @video_style.save
+      render json: @video_style, status: :created, location: @video_style
+    else
+      render json: @video_style.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /video_styles/1
+  def update
+    if @video_style.update(video_style_params)
+      render json: @video_style
+    else
+      render json: @video_style.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /video_styles/1
+  def destroy
+    @video_style.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_video_style
+      @video_style = VideoStyle.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def video_style_params
+      params.permit(
+        :name,
+        :video_comments_count,
+        :video_likes_count,
+        :video_shares_count,
+        :video_url,
+        :thumbnail_url
+      )
+    end
+end
--- app/models/video_style.rb
+++ app/models/video_style.rb
@@ -1,2 +1,9 @@
 class VideoStyle < ApplicationRecord
+  validates :name,
+    :video_url,
+    :video_likes_count,
+    :video_comments_count,
+    :video_shares_count,
+    :thumbnail_url,
+    presence: true
 end
