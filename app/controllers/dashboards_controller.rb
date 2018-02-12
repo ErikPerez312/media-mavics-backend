ed0887e8c02bb5b674ab6ec2c7a56e380d5e8989
ErikPerez312 <theerikperez@gmail.com>
2018-02-12 15:46:53 -0800

    Added Dashboard controller & some routes & Views

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,19 @@
+class DashboardsController < ApplicationController
+  before_action :set_page, only:[:show, :edit, :update, :destroy]
+  skip_before_action :require_login, raise: false
+
+  def index
+    puts "Index called"
+  end
+
+  def show
+    @video_styles = VideoStyle.all
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_page
+      @style = VideoStyle.find(params[:id])
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   resources :users
   resources :campaigns
   resources :video_styles
+
+  get "/dashboard", to: "dashboards#index"
+  get "/dashboard/video_styles", to: "dashboards#show"
+  get "dashboard/video_styles/:id", to: "dashboards#video_style"
+  # resources :dashboards
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
