ea93c4e6cef1a42acc94d1641732b5b0dd2a7167
ErikPerez312 <theerikperez@gmail.com>
2018-02-12 12:12:44 -0800

    Added QR_CODE_SCAN_COUNT property to CAMPAIGN

--- db/migrate/20180211231009_add_qr_code_scan_count_to_campaign.rb
+++ db/migrate/20180211231009_add_qr_code_scan_count_to_campaign.rb
@@ -1,5 +1,5 @@
 class AddQrCodeScanCountToCampaign < ActiveRecord::Migration[5.1]
   def change
-    add_column :campaigns, :qr_code_scan_count, :integer
+    add_column :campaigns, :qr_code_scan_count, :integer, :default => 0
   end
 end
