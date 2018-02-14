38e6f7d2dd99d57686085ed2fac7ed2aeae86348
ErikPerez312 <theerikperez@gmail.com>
2018-02-14 10:07:52 -0800

    Added custom routes for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,24 @@
 Rails.application.routes.draw do
   resources :users
   resources :campaigns
-  resources :video_styles
+
+  resources :video_styles do
+    collection do
+      get "/new", to: "video_styles#new"
+    end
+
+    member do
+      # pages/6/edit
+      get "/edit", to: "video_style#edit", as: "edit"
+    end
+  end
+
 
   get "/dashboard", to: "dashboards#index"
-  get "/dashboard/video_styles", to: "dashboards#show"
-  get "dashboard/video_styles/:id", to: "dashboards#video_style"
+
+  # get "/dashboard/video_styles", to: "dashboards#show"
+
+  # get "dashboard/video_styles/:id", to: "dashboards#video_style"
   # resources :dashboards
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
