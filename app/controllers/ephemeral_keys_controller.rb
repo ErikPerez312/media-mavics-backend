b049d6ed0c9aed416253fa21288d009543b740b9
ErikPerez312 <theerikperez@gmail.com>
2018-03-14 18:32:29 -0700

    Ephemeral Key post route now returns key

--- app/controllers/ephemeral_keys_controller.rb
+++ app/controllers/ephemeral_keys_controller.rb
@@ -1,18 +1,18 @@
 class EphemeralKeysController < ApplicationController
+    require('pp')
   # before_action :set_ephemeral_key, only: [:show, :update, :destroy]
-
-
 
   # POST /ephemeral_keys
   def create
     stripe_version = ephemeral_key_params['api_version']
     customer_id = ephemeral_key_params["customer_id"] # session['customer_id']
 
-    key = Stripe::EphemeralKey.create(
+    @key = Stripe::EphemeralKey.create(
       {customer: customer_id},
       {stripe_version: stripe_version}
     )
-    key.to_json
+    
+    render json: @key, status: :created
   end
 
   private
@@ -20,7 +20,8 @@ class EphemeralKeysController < ApplicationController
     def ephemeral_key_params
       params.permit(
         :customer_id,
-        :api_version
+        :api_version,
+        :ephemeral_key
       )
     end
 end
